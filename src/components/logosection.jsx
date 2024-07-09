7763b14dc6d687224d89d27ebeeb522a38b528e6
Kishaiyan VT <kishaiyanthangaraj@gmail.com>
2024-07-09 16:19:49 +0930

    Changes to the UI

--- src/components/logosection.jsx
+++ src/components/logosection.jsx
@@ -3,14 +3,17 @@ import agri from "../assets/agritech.jpg"
 
 export default function LogoSection(){
   return(
-    <>
+    
    
-      <div>
-      <h1 className="text-3xl text-blue-800">Workshop on AI and Robotics For Future Farming - IROS 2024</h1>
-
+      <div className="relative w-full">
         <img className="object-fill" src={agri} alt="Picture of a drone flying over a Agricultural field" style={{width:"100%",}}/>
+        <div class="absolute inset-0 flex items-left justify-center">
+            <h1 class="text-base md:text-xl lg:text-3xl text-black font-serif">Workshop on AI and Robotics For Future Farming - IROS 2024</h1>
+        </div>
+
+        
       </div>
    
-    </>
+  
   )
 }
