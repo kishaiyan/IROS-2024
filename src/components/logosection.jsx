2964e2cf32363b883c10e697a6e8627c1f2b4270
Kishaiyan VT <kishaiyanthangaraj@gmail.com>
2024-07-18 15:40:13 +0930

    Changing the image for agriTech

--- src/components/logosection.jsx
+++ src/components/logosection.jsx
@@ -1,11 +1,11 @@
 import logo from "../assets/logo.png"
-import agri from "../assets/agritech.jpg"
+import agri from "../assets/Futuristic Farming Scene.webp"
 
 export default function LogoSection(){
   return(
     <>
       <div className="text-center">
-        <h1 className="text-base md:text-xl lg:text-3xl font-serif">Workshop on AI and Robotics For Future Farming - IROS 2024</h1>
+        <h1 className="text-base md:text-xl lg:text-3xl font-serif font-bold">IROS Workshop on AI and Robotics For Future Farming</h1>
         <h1 className="text-blue-600">October 2024</h1>
       </div>
       <div className="flex w-full">
