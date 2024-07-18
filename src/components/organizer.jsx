e256aa627db2c800ea958f42c26bcab69be34b4d
Kishaiyan VT <kishaiyanthangaraj@gmail.com>
2024-07-18 15:29:13 +0930

    Changed the schedule

--- src/components/organizer.jsx
+++ src/components/organizer.jsx
@@ -18,8 +18,9 @@ export default function Organizers(){
       </div>
       <div className="col-span-8 flex flex-col justify-center">
         <h4 className="font-bold">Dr.Yongliang Qiao</h4>
-        <p className="italic font-thin text-sm">The University of Adelaide</p>
-        <a href="https://researchers.adelaide.edu.au/profile/yongliang.qiao" className="text-blue-800 text-xs hover:underline">Personal Website</a>
+        <p className="italic font-thin text-sm mt-1">The University of Adelaide</p>
+        <a href="https://researchers.adelaide.edu.au/profile/yongliang.qiao" className="text-blue-400 text-xs hover:text-blue-800 mt-1">Personal Website</a>
+        <p className="text-sm text-gray-500 mt-1">Email: <a href="mailto:yongliang.qiao@adelaide.edu.au" className="text-gray-500 hover:text-gray-800 text-sm">yongliang.qiao@adelaide.edu.au</a></p>
       </div>
     </div>
     <div className="grid grid-cols-12 gap-3 m-4">
@@ -28,9 +29,10 @@ export default function Organizers(){
   </div>
   <div className="col-span-8 flex flex-col justify-center">
     <h4 className="font-bold">Manoj Karkee</h4>
-    <p className="italic font-thin text-sm">Washington State University</p>
-    <a href="https://bsyse.wsu.edu/people/faculty/karkee/" className="text-blue-800 text-xs hover:underline">Personal Website</a>
-  </div>
+    <p className="italic font-thin text-sm mt-1">Washington State University</p>
+    <a href="https://bsyse.wsu.edu/people/faculty/karkee/" className="text-blue-400 text-xs hover:text-blue-800 mt-1">Personal Website</a>
+    <p className="text-sm text-gray-500 mt-1">Email: <a href="mailto:%20manoj.karkee@wsu.edu" className="text-gray-500 hover:text-gray-800 text-sm">manoj.karkee@wsu.edu</a></p>
+    </div>
 </div>
 <div className="grid grid-cols-12 gap-3 m-4">
   <div className="col-span-4 flex items-center justify-center">
@@ -38,9 +40,9 @@ export default function Organizers(){
   </div>
   <div className="col-span-8 flex flex-col justify-center">
     <h4 className="font-bold">Wessam Essam Elssawy</h4>
-    <p className="italic font-thin text-sm">Agricultural Engineering Research Institute,Egypt</p>
-    <a href="https://portal.arid.my/ar-LY/ApplicationUsers/GetProfile/0006-6094" className="text-blue-800 text-xs hover:underline">Personal Website</a>
-  </div>
+    <p className="italic font-thin text-sm mt-1">Agricultural Engineering Research Institute,Egypt</p>
+    <a href="https://portal.arid.my/ar-LY/ApplicationUsers/GetProfile/0006-6094" className="text-blue-400 text-xs hover:text-blue-800 mt-1">Personal Website</a>
+    </div>
 </div>
 <div className="grid grid-cols-12 gap-3 m-4">
   <div className="col-span-4 flex items-center justify-center">
@@ -48,9 +50,9 @@ export default function Organizers(){
   </div>
   <div className="col-span-8 flex flex-col justify-center">
     <h4 className="font-bold">Alen Alempijevic</h4>
-    <p className="italic font-thin text-sm">University of Technology Sydney</p>
-    <a href="https://profiles.uts.edu.au/Alen.Alempijevic" className="text-blue-800 text-xs hover:underline">Personal Website</a>
-  </div>
+    <p className="italic font-thin text-sm mt-1">University of Technology Sydney</p>
+    <a href="https://profiles.uts.edu.au/Alen.Alempijevic" className="text-blue-500 text-xs hover:text-blue-800 mt-1">Personal Website</a>
+    <p className="text-sm text-gray-500 mt-1">Email: <a href="mailto:Alen.Alempijevic@uts.edu.au" className="text-gray-500 hover:text-gray-800 text-sm">Alen.Alempijevic@uts.edu.au</a></p></div>
 </div>
       </div>
     
--- src/components/schedule.jsx
+++ src/components/schedule.jsx
@@ -23,7 +23,7 @@ export default function Schedule(){
   </thead>
   <tbody>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>8:30 - 8:35...</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>14:00 - 14:05...</strong></td>
       <td className="border-b-2 border-gray-300">All Organizers</td>
       <td className="border-b-2 border-gray-300">Welcome and Introduction</td>
     </tr>
@@ -34,36 +34,36 @@ export default function Schedule(){
       </td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>8:35 - 8:50</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>14:05 - 14:20</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Chanying (Charlie) Li</strong> <br></br>(University of Florida)</td>
       <td className="border-b-2 border-gray-300">Invited talk #1 - <strong>Sensing and Robotics for High-throughput Phenotyping</strong></td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>8:50 - 9:05</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>14:20 - 14:35</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Liangliang Yang</strong> <br></br>(Kitami Institute of Technology)</td>
       <td className="border-b-2 border-gray-300">Invited talk #2 - <strong>Development of agriculture robots using AI for smart agriculture in Japan</strong></td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>9:05 - 9:20</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>14:35 - 14:50</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Dr. João Valente</strong> <br></br> (Spanish National Research Council)</td>
       <td className="border-b-2 border-gray-300">Invited talk #3 - <strong>On-the-Fly Crop Phenotyping Through Close-Proximity Drone Sensing</strong></td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>9:20 - 9:35</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>14:50 - 15:05</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Meili Wang</strong> <br></br>(Northwest A&F University)</td>
       <td className="border-b-2 border-gray-300">Invited talk #4 - <strong>Enhancing Precision Livestock Management with Advanced Machine Vision and Deep Learning Techniques</strong></td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>9:35 - 9:50</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>15:05 - 15:20</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Guoqiang Ren</strong> <br></br>(Zhejiang University)</td>
       <td className="border-b-2 border-gray-300">Invited talk #5 - <strong>Mobile robotic platform for specialty crop cultivation</strong></td>
     </tr>
     <tr className="bg-gray-200">
-      <td className="border-b-2 border-gray-300">9:50 - 10:05</td>
+      <td className="border-b-2 border-gray-300"><strong>15:20 - 15:35</strong></td>
       <td className="border-b-2 border-gray-300 text-center" colSpan="2">3-min Paper/Poster Presentations (breaks included)</td>
     </tr>
     <tr className="bg-green-200">
-      <td className="border-b-2 border-gray-300">10:05 - 10:25</td>
+      <td className="border-b-2 border-gray-300"><strong>15:35 - 15:55</strong></td>
       <td className="border-b-2 border-gray-300 text-center" colSpan="2"><strong>Session 1: Panel Discussion (Moderator: Qiao and Karkee; Panellists: Invited Speakers of Session 1)</strong></td>
     </tr>
     <tr>
@@ -73,55 +73,55 @@ export default function Schedule(){
       </td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>10:25 - 10:40</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>15:55 - 16:10</strong></td>
       <td className="border-b-2 border-gray-300"><strong> Maren Bennewitz</strong> <br></br>(University of Bonn) </td>
       <td className="border-b-2 border-gray-300">Invited talk #6 - <strong>Active Perception for Fruit mapping and Autonomous Harvesting</strong></td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>10:40 - 10:55</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>16:10 - 16:25</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Sven Behnke</strong> <br></br>(University of Bonn)</td>
       <td className="border-b-2 border-gray-300">Invited talk #7 - <strong>In-field High-resolution 3-D Plant Phenotyping</strong></td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>10:55 - 11:10</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>16:25 - 16:40</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Yu Jiang</strong> <br></br>(Cornell University)</td>
       <td className="border-b-2 border-gray-300">Invited talk #5- <strong>Robotic systems to support the development of resilient and sustainable speciality crops</strong></td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>11:10 - 11:25</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>16:40 - 16:55</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Muhammad Naveed Tahir</strong><br></br>(PMAS-Arid Agriculture University)</td>
       <td className="border-b-2 border-gray-300">Invited talks #6 <strong>AI application for weeds, disease detection and management</strong></td>
     </tr>
    
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>11:25 - 11:40</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>16:55 - 17:10</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Henry Gordon Smith</strong> <br></br>(CEO of Agritecture)</td>
       <td className="border-b-2 border-gray-300">Invited talk #7 - <strong>TBD</strong></td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>11:40 - 11:55</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>17:10 - 17:25</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Yazen Al Kodmani</strong> <br></br>(GM of Emirates Bio Farm)</td>
       <td className="border-b-2 border-gray-300">Invited talk #8 - <strong>The future of biofarm(TBD)</strong></td>
     </tr>
     <tr>
-      <td className="border-b-2 border-gray-300"><strong>11:55 - 12:10</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>17:25 - 17:40</strong></td>
       <td className="border-b-2 border-gray-300"><strong>Guoyu Lu</strong> <br></br>(University of Georgia)</td>
       <td className="border-b-2 border-gray-300">Invited talk #9 - <strong>Field Robotics(tentative)</strong></td>
     </tr>
     <tr className="bg-green-300">
-      <td className="border-b-2 border-gray-300"><strong>12:10 - 12:20</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>17:40 - 17:55</strong></td>
       <td className="border-b-2 border-gray-300 text-center" colSpan="2">
         <strong>Session 2: Panel Discussion (Moderator: Allen, Wessam : Invited Speakers of Session 2)</strong>
       </td>
     </tr>
     <tr className="bg-orange-300">
-      <td className="border-b-2 border-gray-300"><strong>12:20 - 12:30</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>17:55 - 18:00</strong></td>
       <td className="border-b-2 border-gray-300 text-center" colSpan="2">
         Final remarks and close out
       </td>
     </tr>
     <tr className="bg-orange-300">
-      <td className="border-b-2 border-gray-300"><strong>12:30 - 13:30</strong></td>
+      <td className="border-b-2 border-gray-300"><strong>18:00 - 19:00</strong></td>
       <td className="border-b-2 border-gray-300 text-center" colSpan="2">
         Networking and Lunch
       </td>
